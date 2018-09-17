7b18eab8aeff4aeebe0517c1eb7be2948d1ab0ba
Jairo Serrato <jairo-serratoo@hotmail.com>
2018-09-17 10:41:41 -0500

    Commit Prueba

---
+++ Frontend/vistas/plantilla.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="title" content="Tienda Virtual">
+	<meta name="description" content="Pagina de venta de articlos, ecommerce">
+	<meta name="keyword" content="articulos, ecommerce">
+
+	<title>Tienda Virtual</title>
+
+	
+
+	<link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+	<link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+	<link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet"> <!--link de la letra ubuntu-->
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu+Condensed" rel="stylesheet"> <!--link de la letra ubuntu condensada-->
+	<link rel="stylesheet" href="vistas/css/plantilla.css"> <!--se linkea la hoja de estilos-->
+	<link rel="stylesheet" href="vistas/css/cabezote.css"> <!--linkeamos la hoja de estilo del cabezote-->
+
+	<script src="main.js"></script>
+	<script src="vistas/js/plugins/jquery.min.js"></script>
+	<script src="vistas/js/plugins/bootstrap.min.js"></script>	
+
+</head>
+<body>
+	<?php
+		//Cabezote
+		include "modulos/cabezote.php";
+	?>
+</body>
+</html>
